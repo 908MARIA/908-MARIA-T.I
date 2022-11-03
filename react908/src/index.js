d32d7fa9eb4587af37f1aeda61524f9b88948d01
908MARIA <maria.guadalupe151022@gmai.com>
2022-11-02 19:11:35 -0600

    paso 1

--- react908/src/index.js
+++ react908/src/index.js
@@ -2,27 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
+  // const-definitions
+  const Header = ''
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const Content = 'Fundamentals of React'
+  const Total = 10 
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      < Content/>
+      < Total/>
     </div>
   )
 }
