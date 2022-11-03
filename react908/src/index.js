4ebf58394a009d75189ef7b466e531ac74d21f19
908MARIA <maria.guadalupe151022@gmai.com>
2022-11-02 23:45:44 -0600

    Ejercicio1.6 unicate paso1

--- react908/src/index.js
+++ react908/src/index.js
@@ -1,38 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 
-const Header = (props) => {
-  console.log(props)
-  return <h1>{props.course}</h1>
-}
-
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
 
 const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+
+  const setToGood = newValue => {
+    setGood(newValue)
   }
+
+  const setToBad = newValue => {
+    setBad(newValue)
+  }
+
+  const setToNeutral = newValue => {
+    setNeutral(newValue)
+  }
+
+  // No defina componentes adentro de otro componente
+  const Display = props => <div>{props.value}</div>
+  const Display2 = props => <div>{props.value2}</div>
+  const Display3 = props => <div>{props.value3}</div>
 
   return (
     <div>
-      ...
+      <Display value={good} />
+      <Display2 value2={neutral} />
+      <Display3 value3={bad} />
+      
+      <Button handleClick={() => setToGood(good + 1)} text="Good" />
+      <Button handleClick={() => setToNeutral(neutral + 1)} text="Neutral" />
+      <Button handleClick={() => setToBad(bad + 1)} text="Bad" />
     </div>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
